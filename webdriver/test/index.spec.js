d10d9aa9385469d9711f5ccd6aafdc8db44cd010
Jon Clerck <j.m.clerck@icloud.com>
2017-04-19 18:52:35 +0100

    👕 Fix linter errors

--- webdriver/test/index.spec.js
+++ webdriver/test/index.spec.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
+const assert = require('assert');
 
 describe('webdriver.io page', function() {
   it('should have the right title - the fancy generator way', function() {
     browser.url('http://webdriver.io');
-    var title = browser.getTitle();
+    const title = browser.getTitle();
     assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
   });
 });
